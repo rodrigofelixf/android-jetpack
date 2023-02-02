6e8466b7eef3dd23304fa8f43f78adde8ae11537
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2023-02-02 16:45:08 -0300

    leaning layouts

--- Compose/app/src/main/java/com/example/compose/MainActivity.kt
+++ Compose/app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +30,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
+}
+
+@Composable
+fun MainScreen() {
+    Column(modifier = Modifier.background(Color.Red)) {
+        Text(
+            text = "Hello World!",
+            modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+        )
+        Text(
+            text = "Outra coisa escrita",
+            modifier = Modifier
+                .background(Color.Yellow)
+                .fillMaxWidth()
+        )
+
+        Row(modifier = Modifier.background(Color.White)) {
+            Text(
+                text = "Text 3",
+                modifier = Modifier
+                    .weight(2f, true),
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "Text 4",
+                modifier = Modifier
+                    .weight(2f),
+                textAlign = TextAlign.Center
+            )
+
+
+        }
+    }
+
+
 }
 
 @Composable
@@ -36,8 +81,9 @@ fun Greeting(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MainScreenPreview() {
     ComposeTheme {
-        Greeting("Android")
+        MainScreen()
+
     }
 }
