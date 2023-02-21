f63834c7e8e2e16e278a42c274823ddee64b470a
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2023-02-21 20:48:33 -0300

    added new project lifecycle

---
+++ Lifecycle/app/src/main/java/br/com/dio/lifecycle/MainActivity.kt
@@ -0,0 +1,42 @@
+package br.com.dio.lifecycle
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import br.com.dio.lifecycle.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.counter.text = counter.toString()
+
+
+        binding.fab.setOnClickListener {
+            counter++
+            binding.counter.text = counter.toString()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
